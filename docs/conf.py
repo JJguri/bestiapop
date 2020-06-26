cc59f01500b83848c11d57269fc94192ff3f662f
darkquasar <darkquasar7@gmail.com>
2020-06-26 00:39:29 -0700

    documenatiton: switching back to rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -86,8 +86,8 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = "pydata_sphinx_theme"
-#html_theme = 'sphinx_rtd_theme'
+#html_theme = "pydata_sphinx_theme"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
