264e636964ee340eed01ab2483d525e8e809ad04
darkquasar <darkquasar7@gmail.com>
2020-08-31 12:33:42 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ Programming Language :: Python
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
-Programming Language :: Python :: 3.8
 Topic :: Scientific/Engineering
 Topic :: Database
 Topic :: Software Development :: Libraries :: Python Modules
